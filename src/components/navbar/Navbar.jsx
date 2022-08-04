7c585805d5ee8f7eea9fad458455e001a6b2b263
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-08-03 20:21:05 -0400

    Fix logo and add scroll use effect

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,29 @@
 import "./navbar.scss";
 import SearchIcon from "@mui/icons-material/Search";
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import logo from '../../images/netflixlogo.png';
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import logo from "../../images/netflixlogo.png";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  // if its not scrolled set background to transparent if scrolled set background to black
+
+  console.log(window.pageYOffset); // this will give us the number of pixels we have scrolled
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null); // this will remove the event listener , clean up function
+  }
+
+  console.log(isScrolled);
+
   return (
     <div className="navbar">
       <div className="container">
         <div className="left">
-          <img
-            src={logo}
-            alt="netflix logo"
-          />
+          <img src={logo} alt="netflix logo" />
           <span>Homepage</span>
           <span>Series</span>
           <span>Movies</span>
@@ -20,12 +31,21 @@ const Navbar = () => {
           <span>My List</span>
         </div>
         <div className="right">
-          <SearchIcon className='icon'/>
+          <SearchIcon className="icon" />
           <span>KID</span>
-          <NotificationsIcon className='icon'/>
-          <img src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png'
-          alt='avatar' />
-          <ArrowDropDownIcon className='icon'/>
+          <NotificationsIcon className="icon" />
+          <img
+            src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png"
+            alt="avatar"
+          />
+          {/* Drop Down Menu */}
+          <div className="profile">
+            <ArrowDropDownIcon className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
