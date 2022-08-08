caf7b563e141a6461f4a84ba73815a3814f6593b
Julia Tran <julia.kim.tran@hotmail.com>
2022-08-08 18:32:10 -0400

    Add button styling

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -2,14 +2,25 @@ import React from 'react'
 import './list.scss'
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { ListItem } from '../listitem/ListItem';
+
 export const List = () => {
   return (
     <div className='list'>
-      <span className='listTitle'>Continue to wartch</span>
+      <span className='listTitle'>Continue to watch</span>
       <div className="wrapper">
         <ArrowBackIosIcon className="sliderArrow left" />
         <div className="container">
-          
+          <ListItem  />
+          <ListItem  />
+          <ListItem  />
+          <ListItem  />
+          <ListItem  />
+          <ListItem  />
+          <ListItem  />
+          <ListItem  />
+          <ListItem  />
+          <ListItem  />
         </div>
         <ArrowForwardIosIcon className="sliderArrow right" />
 
