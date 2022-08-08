b2f9defc5c1f9202d611372031f22cfa6d975eb1
Julia Tran <julia.kim.tran@hotmail.com>
2022-08-08 18:20:17 -0400

    Create list item componenent

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import './list.scss'
-
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 export const List = () => {
   return (
-    <div>List</div>
+    <div className='list'>
+      <span className='listTitle'>Continue to wartch</span>
+      <div className="wrapper">
+        <ArrowBackIosIcon className="sliderArrow left" />
+        <div className="container">
+          
+        </div>
+        <ArrowForwardIosIcon className="sliderArrow right" />
+
+      </div>
+    </div>
   )
 }
---
+++ src/components/listitem/ListItem.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './listitem.scss'
+
+export const ListItem = () => {
+  return (
+    <div className='listItem'>ListItem</div>
+  )
+}
