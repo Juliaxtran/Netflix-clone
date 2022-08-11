2101d559eda4fbe0e40a607e1b923f33310ed512
Julia Tran <julia.kim.tran@hotmail.com>
2022-08-10 22:02:10 -0400

    Move sliders

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -3,14 +3,24 @@ import './list.scss'
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import { ListItem } from '../listitem/ListItem';
+import { useRef } from 'react';
 
 export const List = () => {
+
+  const listRef = useRef();
+
+  const handleClick = (direction) => {
+    // one item 225px and space which 5 px total is 230px
+    if (direction === 'left') {
+
+    }
+  }
   return (
     <div className='list'>
       <span className='listTitle'>Continue to watch</span>
       <div className="wrapper">
-        <ArrowBackIosIcon className="sliderArrow left" />
-        <div className="container">
+        <ArrowBackIosIcon className="sliderArrow left" onClick={() => handleClick("left")}/>
+        <div className="container" ref={listRef}>
           <ListItem  />
           <ListItem  />
           <ListItem  />
@@ -22,7 +32,7 @@ export const List = () => {
           <ListItem  />
           <ListItem  />
         </div>
-        <ArrowForwardIosIcon className="sliderArrow right" />
+        <ArrowForwardIosIcon className="sliderArrow right" onClick={() => handleClick("right")}/>
 
       </div>
     </div>
