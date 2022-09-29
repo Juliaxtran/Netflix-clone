0e69651fe35dd7f8b2baa72214bae393a2222ba5
Julia Tran <julia.kim.tran@hotmail.com>
2022-09-29 12:39:50 -0400

    Add hovering effect

--- src/components/listitem/ListItem.jsx
+++ src/components/listitem/ListItem.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import './listitem.scss'
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import AddIcon from '@mui/icons-material/Add';
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
+import ThumbDownOffAltOutlinedIcon from '@mui/icons-material/ThumbDownOffAltOutlined';
+import { useState } from 'react';
 
-export const ListItem = () => {
+export const ListItem = ({index}) => {
+  const[isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className='listItem'>
+    <div className='listItem'
+    style={{left: isHovered && index * 225 - 50}}
+    onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
       <img src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee" alt="" />
+      <div className="itemInfo">
+        <div className="icons">
+          <PlayArrowIcon className="icon" />
+          <AddIcon className="icon" />
+          <ThumbUpOutlinedIcon className="icon" />
+          <ThumbDownOffAltOutlinedIcon className="icon" />
+        </div>
+        <div className="itemInfoTop">
+          <span>1 hour 14 mins</span>
+          <span className='limit'> +16 </span>
+          <span> 1999</span>
+        </div>
+        <div className="desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+          </div>
+        <div className="genre">Action</div>
+      </div>
     </div>
   )
 }
