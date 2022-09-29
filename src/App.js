f0edd07a4e66bff5313e13a4e93a37fc4919438a
Julia Tran <julia.kim.tran@hotmail.com>
2022-09-29 13:41:28 -0400

    Create Login functionality

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './app.scss';
 import Home from './pages/home/Home';
 import Watch from './pages/watch/Watch';
 import Register from './pages/register/Register';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <Login />
     </div>
   );
 }
